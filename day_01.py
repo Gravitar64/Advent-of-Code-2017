ce9f30282a5a6d82f9ba5381037825e50d7600ad
Gravitar64 <gravitar@web.de>
2023-09-01 13:20:07 +0200

    V2

--- day_01.py
+++ day_01.py
@@ -4,16 +4,16 @@ from time import perf_counter as pfc
 def load(file):
   with open(file) as f:
     return f.read()
-    
-    
 
-def solve(p,n):
-  return sum(int(p[i]) for i in range(l) if p[i] == p[(i+n)%l])
-  
+
+def solve(p, n):
+  return sum(int(p[i]) for i in range(l) if p[i] == p[(i+n) % l])
+
+
 start = pfc()
 puzzle = load('day_01.txt')
-l,n = len(puzzle), len(puzzle)//2
+l, n = len(puzzle), len(puzzle)//2
 
-print(f'Part1: {solve(puzzle,1)}')      
+print(f'Part1: {solve(puzzle,1)}')
 print(f'Part2: {solve(puzzle,n)}')
-print(f'Ermittelt in {pfc()-start:.5f} Sek.')      
+print(f'Ermittelt in {pfc()-start:.5f} Sek.')
