568277229aae5bd745327c89aed2397f202dee83
Gravitar64 <gravitar@web.de>
2023-09-01 14:02:32 +0200

    V2 0,3ms

--- day_02.py
+++ day_02.py
@@ -1,16 +1,16 @@
 from time import perf_counter as pfc
-from itertools import permutations as perm
+from itertools import combinations as comb
 
 
 def load(file):
   with open(file) as f:
-    return [list(map(int,z.split())) for z in f.readlines()]
+    return [sorted([int(n) for n in z.split()],reverse=True) for z in f.readlines()]
 
 
 def solve(p):
-  part1 = sum(max(z)-min(z) for z in p)
-  part2 = sum(a/b for z in p for a,b in perm(z,2) if (a/b).is_integer())
-  return part1, int(part2)
+  part1 = sum(z[0]-z[-1] for z in p)
+  part2 = sum(a//b for z in p for a,b in comb(z,2) if (a/b).is_integer())
+  return part1, part2
     
 
 start = pfc()
