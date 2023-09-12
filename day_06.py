68ae8df15f9525736cc33597b6e17d86ab2eff84
Gravitar64 <gravitar@web.de>
2023-09-12 21:01:11 +0200

    V2 18mx

--- day_06.py
+++ day_06.py
@@ -7,19 +7,17 @@ def load(file):
 
 
 def solve(p):
-  counter, seen =  0, dict()
+  counter, seen, l = 0, dict(), len(p)
   while tuple(p) not in seen:
     seen[tuple(p)] = counter
     m = max(p)
     i = p.index(m)
     p[i] = 0
-    while m > 0:
-      i = (i+1)%len(p)
-      p[i]+= 1
-      m -= 1
+    for j in range(i+1,i+1+m):
+      p[j%l] += 1
     counter += 1
   return counter, counter - seen[tuple(p)]
-  
+
 
 start = pfc()
 p = load('day_06.txt')
