68324311ada7286a992ba838fc4f02100b6bfaa2
Gravitar64 <gravitar@web.de>
2023-10-04 12:36:16 +0200

    V2 ohne Deque

--- day_18.py
+++ day_18.py
@@ -1,48 +1,48 @@
 import time
-from collections import defaultdict, deque
+from collections import defaultdict
 
 
 class Computer():
-  def __init__(self, prg, part1):
+  def __init__(self, prg):
     self.prg = prg
     self.pc = 0
     self.recovered = 0
     self.registers = defaultdict(int)
-    self.queue = deque()
-    self.part1 = part1
+    self.queue = list()
     self.otherComp = None
     self.sendCounter = 0
     self.waiting = False
 
-  def step(self):
+  def step(self, part1):
     def v(val): return self.registers[val] if val.isalpha() else int(val)
 
     opc, *values = self.prg[self.pc].split()
     reg, a = values[0], v(values[0])
     if len(values) == 2: b = v(values[1])
+    
     match opc:
       case 'set': self.registers[reg] = b
       case 'add': self.registers[reg] += b
       case 'mul': self.registers[reg] *= b
       case 'mod': self.registers[reg] %= b
-      case 'snd':
-        self.recovered = a
-        if not self.part1:
-          self.otherComp.queue.append(a)
-          self.sendCounter += 1
-      case 'rcv':
-        if self.part1 and a: return self.recovered
-        if not self.part1:
-          if self.queue:
-            self.registers[reg] = self.queue.popleft()
-            self.waiting = False
-          else:
-            self.waiting = True
-            return
       case 'jgz':
         if a > 0:
           self.pc += b
           return
+      case 'snd':
+        self.recovered = a
+        if not part1:
+          self.otherComp.queue.append(a)
+          self.sendCounter += 1
+      case 'rcv':
+        if part1 and a: return True
+        if self.queue:
+          self.registers[reg] = self.queue.pop(0)
+          self.waiting = False
+        else:
+          self.waiting = True
+          return
+      
     self.pc += 1
 
 
@@ -52,15 +52,15 @@ def load(file):
 
 
 def solve(prg):
-  comp, comp0, comp1 = Computer(prg, True), Computer(prg, False), Computer(prg, False)
+  comp, comp0, comp1 = Computer(prg), Computer(prg), Computer(prg)
   while True:
-    if comp.step(): break
+    if comp.step(True): break
 
   comp0.otherComp, comp1.otherComp = comp1, comp0
   comp1.registers['p'] = 1
   while True:
-    comp0.step()
-    comp1.step()
+    comp0.step(False)
+    comp1.step(False)
     if comp0.waiting and comp1.waiting: break
 
   return comp.recovered, comp1.sendCounter
