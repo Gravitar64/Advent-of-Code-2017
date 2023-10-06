f0c1b0b93fa8e28ae9c80638a180c4f389af3310
Gravitar64 <gravitar@web.de>
2023-10-06 10:22:23 +0200

    V2 Solve-Parameter geändert

--- day_22.py
+++ day_22.py
@@ -6,18 +6,13 @@ def load(file):
     return [row.strip() for row in f.readlines()]
 
 
-def solve(puzzle, first_part):
-  grid = {x + 1j * y: 'i' for y,row in enumerate(puzzle) for x, c in enumerate(row) if c == '#'}
-  p1 = dict(c=(-1j, 'i', 1), i=(1j, 'c', 0))
-  p2 = dict(c=(-1j, 'w', 0), w=(1, 'i', 1), i=(1j, 'f', 0), f=(-1, 'c', 0))
-
+def solve(puzzle, steps, rules, infected=0):
+  grid = {x + 1j * y: 'i' for y, row in enumerate(puzzle) for x, c in enumerate(row) if c == '#'}
   facing = 0 + -1j
   node = len(puzzle[0]) // 2 + 1j * (len(puzzle) // 2)
-  infected = 0
-  sims = 10_000 if first_part else 10_000_000
-
-  for _ in range(sims):
-    rot, new_state, add = p1[grid.get(node, 'c')] if first_part else p2[grid.get(node, 'c')]
+  
+  for _ in range(steps):
+    rot, new_state, add = rules[grid.get(node, 'c')]
     facing *= rot
     grid[node] = new_state
     infected += add
@@ -27,6 +22,9 @@ def solve(puzzle, first_part):
 
 start = time.perf_counter()
 puzzle = load('day_22.txt')
-print(f'Part 1: {solve(puzzle,True)}')
-print(f'Part 2: {solve(puzzle,False)}')
+p1 = dict(c=(-1j, 'i', 1), i=(1j, 'c', 0))
+p2 = dict(c=(-1j, 'w', 0), w=(1, 'i', 1), i=(1j, 'f', 0), f=(-1, 'c', 0))
+
+print(f'Part 1: {solve(puzzle, 10_000, p1)}')
+print(f'Part 2: {solve(puzzle, 10_000_000, p2)}')
 print(f'Ermittelt in {time.perf_counter()-start:.5f} Sek.')
